bd2b0ffa4867bf1496393dfbdab685f0d0a723c6
pdaniel37 <pedrodaniel.unb@gmail.com>
2018-04-06 18:26:22 -0300

    [UPDATE] Adding some functions

--- src/components/Users/EditUser.vue
+++ src/components/Users/EditUser.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="CreateUser">
+      <input type="text" v-model="input.id" placeholder="Id" />
+      <input type="text" v-model="input.email" placeholder="Email" />
       <input type="text" v-model="input.username" placeholder="Username" />
       <input type="password" v-model="input.password" placeholder="Password" />
-      <input type="text" v-model="input.email" placeholder="Email" />
-      <button v-on:click="sendData()">Send</button>
-      <button v-on:click="getData()">Get</button>
+      <button v-on:click="editData()">Edit</button>
 
       <br />
       <br />
-      <p> {{jsons}} </p>
   </div>
 </template>
 
@@ -19,39 +18,41 @@ export default {
   data () {
     return {
       input: {
+        id: '',
         username: '',
         password: '',
         email: ''
       },
-      response_get: '',
-      response_post: '',
-      jsons: ''
+      response_put: '',
+      response_get: ''
     }
   },
   methods: {
-    sendData () {
+    // getData () {
+    //   this.$http
+    //     .get('http://localhost:8000/users/', {
+    //       headers: { 'content-type': 'application/json' }
+    //     })
+    //     .then(
+    //       result => {
+    //         this.email = this.input.email,
+    //         this.response_get = result.data,
+    //         this.id = this.response_get.find(this.response_get, this.response_get.email == this.email)
+    //         // this.id = this.response_get.email.indexOf(this.email)
+    //       },
+    //       error => {
+    //         console.error(error)
+    //       }
+    //     )
+    // },
+    editData () {
       this.$http
-        .post('http://localhost:8000/users/', this.input, {
+        .put('http://localhost:8000/users/6/', this.input, {
           headers: { 'content-type': 'application/json' }
         })
         .then(
           result => {
             this.response_post = result.data
-          },
-          error => {
-            console.error(error)
-          }
-        )
-    },
-    getData () {
-      this.$http
-        .get('http://localhost:8000/users/', {
-          headers: { 'content-type': 'application/json' }
-        })
-        .then(
-          result => {
-            this.response_get = result.data
-            this.jsons = result.json()
           },
           error => {
             console.error(error)
